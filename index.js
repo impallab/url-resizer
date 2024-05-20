3a55e97b11caae5b51bf5e7f02bdd2fe2cfd6b6c
impallab <pallab13nayagram@gmail.com>
2024-05-21 02:14:11 +0530

    created frontend (UI), few css fix are needed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const staticRouter = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
@@ -10,11 +12,19 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected")
 );
 
+// ejs setup
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRouter);
+
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
@@ -31,4 +41,4 @@ app.get("/:shortId", async (req, res) => {
   res.redirect(entry.redirectURL);
 });
 
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
